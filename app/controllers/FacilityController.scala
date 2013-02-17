9249e2a926e5a7b74bd5a773ae1db2d3420ae0ff
sbasinge@gmail.com <sbasinge@gmail.com>
2013-02-17 17:51:29 -0500

    List and edit facilities

--- app/controllers/FacilityController.scala
+++ app/controllers/FacilityController.scala
@@ -9,13 +9,11 @@ import play.api.data.validation.Constraints._
 import org.squeryl.PrimitiveTypeMode._
 import views._
 import models._
-import scala.util.matching.Regex
 
 object FacilityController extends Controller {
 
   val phoneRegex = "1?\\s*\\W?\\s*([2-9][0-8][0-9])\\s*\\W?\\s*([2-9][0-9]{2})\\s*\\W?\\s*([0-9]{4})(\\se?x?t?(\\d*))?"
-  val matcher:Regex = phoneRegex.r
-    
+
   /**
    * This result directly redirect to the application home.
    */
@@ -24,39 +22,60 @@ object FacilityController extends Controller {
   def list = Action {
     Logger.info("Called facility controller .list")
     inTransaction {
-      val allFacilities = from(AppDB.facilities)(f => select(f))
+      val allFacilities = from(AppDB.facilities)(f => select(f)).seq
       Logger.info(allFacilities.size + " facilities found")
+      Ok(html.facilities.list("Your new application is ready.", allFacilities))
     }
 
-    Ok(
-      views.html.facilities.list(
-        "Your new application is ready."))
   }
 
   def facilityForm = Form(
     mapping(
       "name" -> nonEmptyText,
-      "phone" -> text.verifying (pattern(phoneRegex.r,"constraint.phone","error.phone"))
-    )
-    (Facility.apply)(Facility.unapply)
-  )
+      "phone" -> text.verifying(pattern(phoneRegex.r, "constraint.phone", "error.phone")))(Facility.apply)(Facility.unapply))
 
   def create = Action {
-    Ok(views.html.facilities.createForm(facilityForm))
+    Ok(html.facilities.form(facilityForm, "Add"))
   }
 
   /**
-   * Handle the 'new computer form' submission.
+   * Handle the 'new facility form' submission.
    */
-  def save = Action { implicit request =>
+  def save(messageType: String) = Action { implicit request =>
     facilityForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(views.html.facilities.createForm(formWithErrors)),
-      facility => {
-        inTransaction {
-	        AppDB.facilities.insert(facility)
-	        Home.flashing("success" -> "facility %s has been created".format(facility.name))
-        }
-      })
+      formWithErrors => BadRequest(html.facilities.form(formWithErrors, messageType)),
+        facility =>
+          {
+            inTransaction {
+            	if (messageType == "Add") {
+            		AppDB.facilities.insert(facility)
+            		Home.flashing("success" -> "facility %s has been created".format(facility.name))
+            	} else {
+            		Logger.info(" About to update facility %s".format(facility))
+            	  AppDB.facilities.update(facility)
+            	  Home.flashing("success" -> "facility %s has been updated".format(facility.name))
+            	} 
+            }
+          }
+      )
   }
+
+  def edit(id: Long) = Action {
+    inTransaction {
+      val existingFacility = AppDB.facilities.get(id)
+      Ok(html.facilities.form(facilityForm.fill(existingFacility), "Edit"))
+    }
+  }
+
+  //  def update = Action { implicit request =>
+  //    facilityForm.bindFromRequest.fold(
+  //      formWithErrors => BadRequest(html.facilities.form(formWithErrors,"Edit")),
+  //      facility => {
+  //        inTransaction {
+  //          AppDB.facilities.insert(facility)
+  //          Home.flashing("success" -> "facility %s has been created".format(facility.name))
+  //        }
+  //      })
+  //  }
 
 }
